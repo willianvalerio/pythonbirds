cb8426e9c6a23d5a961e1789dccbb14ee9c0ffc9
willianvalerio <willianvalerio@outlook.com>
2020-04-11 17:33:33 -0300

    Atributos de classe

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -9,6 +9,14 @@ class Pessoa:
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
+    @staticmethod
+    def metodo_estatico():
+        return 42
+
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f'{cls} = - olhos {cls.olhos}'
+
 if __name__ == '__main__':
     renzo = Pessoa(nome='Renzo')
     luciano = Pessoa(renzo,nome='Luciano')
@@ -30,5 +38,7 @@ if __name__ == '__main__':
     print(renzo.olhos)
     print(luciano.olhos)
     print(id(Pessoa.olhos),id(renzo.olhos),id(luciano.olhos))
+    print(Pessoa.metodo_estatico(), luciano.metodo_estatico())
+    print(Pessoa.nome_e_atributos_de_classe(), luciano.nome_e_atributos_de_classe())
 
 
